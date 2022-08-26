fe1e1cc22245b503e5f91b9d8824de23d539b030
tomascone <tomascone13@gmail.com>
2022-08-26 17:56:33 -0400

    CRUD de empleados con livewire

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//Route Hooks - Do not delete//
+	Route::view('empleados', 'livewire.empleados.index')->middleware('auth');
